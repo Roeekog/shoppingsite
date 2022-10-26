59febe1bf806d6e1bce5bbf7b19d08e34b998c8e
Roeekog <roeekog@gmail.com>
2022-10-26 20:47:05 +0300

    created a productslist method in array

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // creating elemnts as div
 const productsContainer = document.getElementById('products-container');
+
+const productsList = ["Black Tea", "Green Tea"];
+
 const blackTea = document.createElement('div');
 const greenTea = document.createElement('div');
 
 // naming the new divs with text
-blackTea.innerHTML = 'Black Tea';
-greenTea.innerHTML = 'Green Tea';
+blackTea.innerHTML = productsList[0];
+greenTea.innerHTML = productsList[1];
 
 // creating a class for the new divs
 blackTea.classList.add('product');
