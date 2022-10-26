41879e7fd048f9688aa7e2aaf3f50b4f76be235d
Roeekog <roeekog@gmail.com>
2022-10-26 20:00:08 +0300

    added dynamic html in js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+// creating elemnts as div
+const productsContainer = document.getElementById('products-container');
+const blackTea = document.createElement('div');
+const greenTea = document.createElement('div');
+
+// naming the new divs with text
+blackTea.innerHTML = 'Black Tea';
+greenTea.innerHTML = 'Green Tea';
+
+// creating a class for the new divs
+blackTea.classList.add('product');
+greenTea.classList.add('product');
+
+// appending the new divs to the product container
+productsContainer.appendChild(blackTea);
+productsContainer.appendChild(greenTea);
